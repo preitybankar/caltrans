3c12bab123668be04cf66237d9ff812e03e5f813
Priti <preity.bankar@gmail.com>
2021-04-02 18:18:28 -0700

    LS models updated

--- WebApp/WebContent/Js/calculate_soil_loss.js
+++ WebApp/WebContent/Js/calculate_soil_loss.js
@@ -4,39 +4,44 @@
  	////////////// Load LS Values for Pre Construction //////////////
 	
 	var lsValueMap = new Map(); // Global variable
+	var isLoading = false;	// Global variable
 	
 	$(document).on("click", "#pre_ls_button", function () {
-    	$.ajax({
-			type: 'GET',
-	        url: 'ls',
-	        async : true,
-	    }).done(function(lsList) {
-	    	var slopeSet = new Set();
-	    	var slopeLengthSet = new Set();
-	    	lsValueMap.clear();
-	    	for (var i = 0; i < lsList.length ; i++ ) {
-	    		var ls = lsList[i];
-	    		slopeSet.add(ls.slope);
-	    		slopeLengthSet.add(ls.slope_length);
-	    		
-	    		var key = constructLSMapKey(ls.slope, ls.slope_length);
-	    		lsValueMap[key] = ls.ls_value;
-	    	}
-	    	slopeSet.forEach(function(slope) {
-	    		$("#pre_slope").append("<option value='"+slope+"'>"+slope+"</option>");
-	    	});
-	    	slopeLengthSet.forEach(function(slope_length) {
-	    		$("#pre_slope_length").append("<option value='"+slope_length+"'>"+slope_length+"</option>");
-	    	});
-	    	
-	    	// Update Pre LS value for the current slope and length
-	    	var slope = $("#pre_slope").val();
-	    	var slopeLength = $("#pre_slope_length").val();
-	    	setPreLSValue(slope, slopeLength);
-	    	
-	    }).fail(function(response) {
-	    	alert(response.responseText);
-	    });
+		if (!isLoading) {
+	        $("#pre_slope").empty();
+	        $("#pre_slope_length").empty();
+	        isLoading = true;   
+	        $.ajax({
+				type: 'GET',
+		        url: 'ls',
+		        async : true,
+		    }).done(function(lsList) {
+		    	var slopeSet = new Set();
+		    	var slopeLengthSet = new Set();
+		    	lsValueMap.clear();
+		    	for (var i = 0; i < lsList.length ; i++ ) {
+		    		var ls = lsList[i];
+		    		slopeSet.add(ls.slope);
+		    		slopeLengthSet.add(ls.slope_length);
+		    		
+		    		var key = constructLSMapKey(ls.slope, ls.slope_length);
+		    		lsValueMap[key] = ls.ls_value;
+		    	}
+		    	slopeSet.forEach(function(slope) {
+		    		$("#pre_slope").append("<option value='"+slope+"'>"+slope+"</option>");
+		    	});
+		    	slopeLengthSet.forEach(function(slope_length) {
+		    		$("#pre_slope_length").append("<option value='"+slope_length+"'>"+slope_length+"</option>");
+		    	});   	
+		    	// Update Pre LS value for the current slope and length
+		    	var slope = $("#pre_slope").val();
+		    	var slopeLength = $("#pre_slope_length").val();
+		    	setPreLSValue(slope, slopeLength);
+		    	isLoading = false;	
+		    }).fail(function(response) {
+		    	alert(response.responseText);
+		    });      
+    	}  	
 	});
 
 	
@@ -55,7 +60,6 @@
 	function setPreLSValue(slope, slopeLength) {
 		var key = constructLSMapKey(slope, slopeLength);
 		var ls = lsValueMap[key];
-		
 		$("#pre_ls_value").text((ls == null) ? "" : ls);
 		$('#select_pre_ls_button').attr("disabled", (ls == null));
 	}
@@ -72,37 +76,41 @@
 	////////////// Load LS Values for Post - Construction ////////////////
 	
 	$(document).on("click", "#post_ls_button", function () {
-    	$.ajax({
-			type: 'GET',
-	        url: 'ls',
-	        async : true,
-	    }).done(function(lsList) {
-	    	var slopeSet = new Set();
-	    	var slopeLengthSet = new Set();
-	    	lsValueMap.clear();
-	    	for (var i = 0; i < lsList.length ; i++ ) {
-	    		var ls = lsList[i];
-	    		slopeSet.add(ls.slope);
-	    		slopeLengthSet.add(ls.slope_length);
-	    		
-	    		var key = constructLSMapKey(ls.slope, ls.slope_length);
-	    		lsValueMap[key] = ls.ls_value;
-	    	}
-	    	slopeSet.forEach(function(slope) {
-	    		$("#post_slope").append("<option value='"+slope+"'>"+slope+"</option>");
-	    	});
-	    	slopeLengthSet.forEach(function(slope_length) {
-	    		$("#post_slope_length").append("<option value='"+slope_length+"'>"+slope_length+"</option>");
-	    	});
-	    	
-	    	// Update Post LS value for the current slope and length
-	    	var slope = $("#post_slope").val();
-	    	var slopeLength = $("#post_slope_length").val();
-	    	setPostLSValue(slope, slopeLength);
-	    	
-	    }).fail(function(response) {
-	    	alert(response.responseText);
-	    });
+	    if (!isLoading) {
+	        $("#post_slope").empty();
+	        $("#post_slope_length").empty();
+	        isLoading = true;
+	       	$.ajax({
+				type: 'GET',
+		        url: 'ls',
+		        async : true,
+		    }).done(function(lsList) {
+		    	var slopeSet = new Set();
+		    	var slopeLengthSet = new Set();
+		    	lsValueMap.clear();
+		    	for (var i = 0; i < lsList.length ; i++ ) {
+		    		var ls = lsList[i];
+		    		slopeSet.add(ls.slope);
+		    		slopeLengthSet.add(ls.slope_length);
+		    		
+		    		var key = constructLSMapKey(ls.slope, ls.slope_length);
+		    		lsValueMap[key] = ls.ls_value;
+		    	}
+		    	slopeSet.forEach(function(slope) {
+		    		$("#post_slope").append("<option value='"+slope+"'>"+slope+"</option>");
+		    	});
+		    	slopeLengthSet.forEach(function(slope_length) {
+		    		$("#post_slope_length").append("<option value='"+slope_length+"'>"+slope_length+"</option>");
+		    	});	    	
+		    	// Update Post LS value for the current slope and length
+		    	var slope = $("#post_slope").val();
+		    	var slopeLength = $("#post_slope_length").val();
+		    	setPostLSValue(slope, slopeLength);
+		    	isLoading = false;
+		    }).fail(function(response) {
+		    	alert(response.responseText);
+		    });
+	    }	
 	}); 
 	
 	////////////// Set/Update LS Value for Post - Construction //////////////
