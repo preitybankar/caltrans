f7a97b565d52610d387e2433db04e754204d706c
Priti <preity.bankar@gmail.com>
2021-05-12 11:48:29 -0700

    Project report completed

--- WebApp/WebContent/Js/report.js
+++ WebApp/WebContent/Js/report.js
@@ -8,9 +8,6 @@ window.onload = function() {
 		var id = paramsArray[0].split('=')[1];
 		var statusResp = paramsArray[1].split('=')[1];
 		var message = paramsArray[2].split('=')[1];
-		//alert("id : " + id);
-		//alert("statusResp : " + statusResp);
-
 		if (statusResp) {
 			$('#message').html("<span><strong>Well done!</strong> Project data record " + message + " successfully." + "</span>")
 				.addClass("alert alert-success")
@@ -54,64 +51,90 @@ window.onload = function() {
 				$("#postKValue").text(site.post_soil_loss.k);
 				$("#preAvalue").text(site.pre_soil_loss.a);
 				$("#postAvalue").text(site.post_soil_loss.a);
+
+				// changeIds("pre", index);
+				// changeIds("post", index);
 				
 				let div = document.createElement('div');
-				div.innerHTML = document.getElementById('siteElements').innerHTML;
-				div.id = "segmentItem-" + index;
+				let siteElementId = "siteElement-" + index;
+				div.innerHTML = document.getElementById('siteElement').innerHTML;
+				div.id = siteElementId;
 				div.className = "container mb-3 border border-warning";
 				
 				let siteReportContainer = document.getElementById('siteReportContainer');
 				siteReportContainer.appendChild(div);
-					
-				//let siteReportContainer = document.getElementById('preCoversTable');
-				let preCoversBody = "preCoversBody_" + index;
-				let postCoversBody = "postCoversBody_" + index;
-				let prePracticesBody = "prePracticesBody_" + index;
-				let postPracticesBody = "postPracticesBody_" + index;
 				
-				$("#preCoversBody").prop("id", preCoversBody);
-				$("#postCoversBody").prop("id", postCoversBody);
-				$("#prePracticesBody").prop("id", prePracticesBody);
-				$("#postPracticesBody").prop("id", postPracticesBody);
+				// Load Pre Covers
+				// let preCoversTableId = "preCoversTable_" + index;
+				// $("#" + siteElementId).find('#preCoversTable').prop("id", preCoversTableId);
 				
-				loadCovers(site.pre_soil_loss, preCoversBody, index);
-				loadCovers(site.post_soil_loss, postCoversBody, index);
-				loadPractices(site.pre_soil_loss, prePracticesBody, index);
-				loadPractices(site.post_soil_loss, postPracticesBody, index);
-	
+				// let preCoversBodyId = "preCoversBody_" + index;
+				// $("#" + preCoversTableId).find('#preCoversBody').prop("id", preCoversBodyId);
+				
+				let preCoversBodyId = changeIds("pre", "Covers", index, siteElementId);
+				loadCovers(site.pre_soil_loss, preCoversBodyId);
+				
+				// Load Post Covers
+				// let postCoversTableId = "postCoversTable_" + index;
+				// $("#" + siteElementId).find('#postCoversTable').prop("id", postCoversTableId);
+				
+				// let postCoversBodyId = "postCoversBody_" + index;
+				// $("#" + postCoversTableId).find('#postCoversBody').prop("id", postCoversBodyId);
+				let postCoversBodyId = changeIds("post", "Covers", index, siteElementId);
+				loadCovers(site.post_soil_loss, postCoversBodyId);
+				
+				// Load Post Practices
+				// let prePracticesTableId = "prePracticesTable_" + index;
+				// $("#" + siteElementId).find('#prePracticesTable').prop("id", prePracticesTableId);
+				
+				// let prePracticesBodyId = "prePracticesBody_" + index;
+				// $("#" + prePracticesTableId).find('#prePracticesBody').prop("id", prePracticesBodyId);
+				let prePracticesBodyId = changeIds("pre", "Practices", index, siteElementId);
+				loadPractices(site.pre_soil_loss, prePracticesBodyId);
+				
+				// Load Post Practices
+				// let postPracticesTableId = "postPracticesTable_" + index;
+				// $("#" + siteElementId).find('#postPracticesTable').prop("id", postPracticesTableId);
+				
+				// let postPracticesBodyId = "postPracticesBody_" + index;
+				// $("#" + postPracticesTableId).find('#postPracticesBody').prop("id", postPracticesBodyId);
+				
+				let postPracticesBodyId = changeIds("post", "Practices", index, siteElementId);
+				loadPractices(site.post_soil_loss, postPracticesBodyId);
 			});	
 		}).fail(function(response) {
-			//alert(response.responseText);
+			 alert(response.responseText);
 		});
 	}
 };
 
-
+function changeIds(type, itemToLoad, siteIndex, siteElementId) {
+	let tableId = type + itemToLoad + "Table_" + siteIndex;
+	
+	$("#" + siteElementId).find('#' + type + itemToLoad + 'Table').prop("id", tableId);
+				
+	let bodyId = type + itemToLoad + "Body_" + siteIndex;
+	$("#" + tableId).find('#' + type + itemToLoad + 'Body').prop("id", bodyId);
+	
+	return bodyId;
+}
 
 // Load covers			
-function loadCovers(soilLoss, tableId, siteId) {
+function loadCovers(soilLoss, tableId) {
 	soilLoss.covers.forEach((cover, index) => {
-		addRow(tableId, (index+1), cover.bmp_name, cover.c_value, cover.percentage, cover.reference, siteId);	
+		addRow(tableId, (index+1), cover.bmp_name, cover.c_value, cover.percentage, cover.reference);	
 	});
 }
-
-function loadPractices(soilLoss, tableId, siteId) {
+// Load practices	
+function loadPractices(soilLoss, tableId) {
 	soilLoss.practices.forEach((practice, index) => {
-		addRow(tableId, (index+1), practice.supportpractices_name, practice.p_value, practice.percentage, practice.reference, siteId);	
+		addRow(tableId, (index+1), practice.supportpractices_name, practice.p_value, practice.percentage, practice.reference);	
 	});
 }
 
-function addRow(tableId, index, name, value, percent, reference, siteId) {
-	// $("#" + tableId).prop("id", tableId + "-" + siteId);
-	//let newTableId = tableId + "-" + siteId;
-	//alert(newTableId);
-	//$("#" + tableId).attr("id", newTableId);
-	
-	//return;
-	
+function addRow(tableId, index, name, value, percent, reference) {
 	// Get a reference to the table
 	let tableRef = document.getElementById(tableId);
-	//alert(tableId);
 	// Insert a row at the end of the table
 	let newRow = tableRef.insertRow(-1);
 	// Insert a cell in the row at index 0
@@ -120,7 +143,6 @@ function addRow(tableId, index, name, value, percent, reference, siteId) {
 	let newCell3 = newRow.insertCell(2);
 	let newCell4 = newRow.insertCell(3);
 	let newCell5 = newRow.insertCell(4);
-
 	// Append a text node to the cell
 	newCell1.appendChild(document.createTextNode(index));
 	newCell2.appendChild(document.createTextNode(name));
